55d0be2b7f7e1f96daecafae6e29d693f6127027
natanael.vieira <natanaelvieira130@gmail.com>
2019-03-21 13:09:46 -0300

    Department entity class

--- src/application/Program.java
+++ src/application/Program.java
@@ -1,11 +1,13 @@
 package application;
 
-
+import model.entities.Department;
 
 public class Program {
 
 	public static void main(String[] args){
+		Department dep = new Department(1,"BOOK");
 		
+		System.out.println(dep);
 	}
 }
 
