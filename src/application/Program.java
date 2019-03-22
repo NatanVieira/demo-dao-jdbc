4093d2d5433296e75194271b8dee19a5d0351911
natanael.vieira <natanaelvieira130@gmail.com>
2019-03-21 21:00:04 -0300

    findById implementation

--- src/application/Program.java
+++ src/application/Program.java
@@ -1,23 +1,20 @@
 package application;
 
 import java.text.ParseException;
-import java.text.SimpleDateFormat;
 
 import model.dao.DaoFactory;
 import model.dao.SellerDao;
-import model.entities.Department;
 import model.entities.Seller;
 
 public class Program {
 
 	public static void main(String[] args) throws ParseException{
-		SimpleDateFormat sdf = new SimpleDateFormat("dd/MM/yyyy");
-		Department dep = new Department(1,"BOOK");
-		Seller sell = new Seller(1, "Natanael", "natanael.vieira@gelopar.com.br", sdf.parse("26/01/1995"), 3000.00,dep);
 		
 		SellerDao sellerDao = DaoFactory.createSellerDao();
-		System.out.println(dep);
-		System.out.println(sell);
+		
+		Seller seller = sellerDao.findById(3);
+		
+		System.out.println(seller);
 	}
 }
 
