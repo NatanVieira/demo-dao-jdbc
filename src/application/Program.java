aff2f7769c9c88b357e750fd4d5f2a030f4b9483
natanael.vieira <natanaelvieira130@gmail.com>
2019-03-25 21:02:30 -0300

    update implementation

--- src/application/Program.java
+++ src/application/Program.java
@@ -28,11 +28,18 @@ public class Program {
 		List<Seller> list = sellerDao.findAll();
 		for(Seller obj: list) {
 			System.out.println(obj);
-		}*/
+		}
 		System.out.println(" === TESTE 4: seller inserts === ");
 		Seller newSeller = new Seller(null, "greg", "greg@gmail.com", new Date(), 4000.00, department);
 		sellerDao.insert(newSeller);
 		System.out.println("Inserted! new Seller id = " + newSeller.getId());
+		*/
+		
+		System.out.println(" === TESTE 4: seller update === ");
+		Seller seller = sellerDao.findById(1);
+		seller.setName("Martha Waine");
+		sellerDao.update(seller);
+		System.out.println("update complete");
 	}
 }
 
